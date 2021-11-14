ab9e8c987e5b16888e8e424e8b6abbc9bb7d0eac
ABN <87401136+abnenov@users.noreply.github.com>
2021-11-14 18:13:14 +0200

    Add files via upload

--- project/Unit1.pas
+++ project/Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,
+  System.Classes, Vcl.Graphics, Math,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.GIFImg,
   Vcl.StdCtrls, Vcl.Imaging.pngimage, Vcl.Imaging.jpeg;
 
@@ -19,12 +19,20 @@ type
     Label5: TLabel;
     Label6: TLabel;
     Timer2: TTimer;
+    Timer3: TTimer;
+    ep1: TImage;
+    enemyMove: TTimer;
+    ep2: TImage;
     procedure FormCreate(Sender: TObject);
-    procedure Timer1Timer(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
     procedure FormKeyPress(Sender: TObject; var Key: Char);
     function inHorizontalCorridor(): boolean;
     function inVerticalCorridor(): boolean;
+    function enemyInHorizontalCorridor(enemy: integer): boolean;
+    function enemyInVerticalCorridor(enemy: integer): boolean;
+    procedure Timer1Timer(Sender: TObject);
+    procedure Timer3Timer(Sender: TObject);
+    procedure enemyMoveTimer(Sender: TObject);
   private
     { Private declarations }
   public
@@ -32,41 +40,48 @@ type
   end;
 
 Const
-  imgHeight = 116;
+  imgHeight = 100;
   imgWidth = 100;
-  horizontalImageStep = 60;
-  verticalImageStep = 45;
+  horizontalBlockStep = 60;
+  verticalBlockStep = 60;
   leftImageOffset = 80;
   topImageOffset = 60;
   gridImageTimerInterval = 70;
-  shipHeight = 55;
-  shipWidth = 55;
-  endLeft = 175;
-  endRight = 975;
-  endUp = 166;
-  endDown = 806;
-  moveStep = 10;
+  shipHeight = 82;
+  shipWidth = 82;
 
 var
   Form1: TForm1;
-  gridImages: array [0 .. 5, 0 .. 6, 0 .. 78] of TImage;
-  imgTimers: array [0 .. 5, 0 .. 6] of integer;
+  gridBlocks: array [0 .. 5, 0 .. 6] of TImage;
   shipX, shipY: integer;
-  moveLeft, moveRight, moveUp, moveDown: Char;
+  ep: array [0 .. 10] of TImage;
+  epX: array [0 .. 10] of integer;
+  epY: array [0 .. 10] of integer;
+  lastMove: array [0 .. 10] of integer;
+  moveLeft, moveRight, moveLeft1, moveRight1, moveUp, moveDown: Char;
   shotLeft, shotRight, shotUp, shotDown, moveStop: Char;
   lastMoveKey, lastShotKey, currentMoveKey: Char;
   verticalCorridors: array [0 .. 5] of integer;
   horizontalCorridors: array [0 .. 4] of integer;
+  endLeft, endRight, endUp, endDown, moveStep: integer;
+  appPath: String;
 
 implementation
 
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
+
 var
-  row, column, image: integer;
+  row, column, blockNumber, i: integer;
   pic: string;
 begin
+  appPath := ExtractFileDir(ExtractFilePath(ParamStr(0)));
+  pic := appPath + '\images\block\block.gif';
+  Ship.Height := shipHeight;
+  Ship.Width := shipWidth;
+  moveLeft1 := 'j';
+  moveRight1 := 'l';
   moveLeft := 'j';
   moveRight := 'l';
   moveUp := 'i';
@@ -76,68 +91,78 @@ begin
   shotUp := 'i';
   shotDown := 'k';
   moveStop := chr(32);
-  verticalCorridors[0] := 175;
-  verticalCorridors[1] := 335;
-  verticalCorridors[2] := 495;
-  verticalCorridors[3] := 655;
-  verticalCorridors[4] := 815;
-  verticalCorridors[5] := 975;
-  horizontalCorridors[0] := 166;
-  horizontalCorridors[1] := 326;
-  horizontalCorridors[2] := 486;
-  horizontalCorridors[3] := 646;
-  horizontalCorridors[4] := 806;
+
+  blockNumber := 0;
   for row := 0 to 5 do
   begin
     for column := 0 to 6 do
     begin
-      randomize;
-      imgTimers[row, column] := random(39);
-      Timer1.Interval := gridImageTimerInterval;
-      for image := 0 to 39 do
-      begin
-        gridImages[row, column, image] := TImage.Create(self);
-        gridImages[row, column, image].Parent := self;
-        gridImages[row, column, image].SendToBack;
-        gridImages[row, column, image].AutoSize := FALSE;
-        gridImages[row, column, image].Height := imgHeight;
-        gridImages[row, column, image].Width := imgWidth;
-        gridImages[row, column, image].Top := topImageOffset + row *
-          (gridImages[row, column, image].Height + verticalImageStep);
-        gridImages[row, column, image].Left := leftImageOffset + column *
-          (gridImages[row, column, image].Width + horizontalImageStep);
-        gridImages[row, column, image].Visible := true;
-        gridImages[row, column, image].Stretch := true;
-        gridImages[row, column, image].Proportional := true;
-        gridImages[row, column, image].Name := 'image' + inttostr(row) +
-          inttostr(column) + inttostr(image);
-        pic := 'C:\Users\ANenov\Documents\CrossFire\frames\' +
-          inttostr(image) + '.gif';
-        gridImages[row, column, image].Picture.LoadFromFile(pic);
-        // imgs[c].OnClick := imgClick;
-        gridImages[row, column, image].Refresh;
-        gridImages[row, column, image].Picture.LoadFromFile(pic);
-        gridImages[row, column, image].Visible := true;
-        gridImages[row, column, image].Refresh;
-      end;
+      gridBlocks[row, column] := TImage.Create(self);
+      gridBlocks[row, column].Parent := self;
+      gridBlocks[row, column].SendToBack;
+      gridBlocks[row, column].AutoSize := FALSE;
+      gridBlocks[row, column].Height := imgHeight;
+      gridBlocks[row, column].Width := imgWidth;
+      gridBlocks[row, column].Top := topImageOffset + row *
+        (gridBlocks[row, column].Height + verticalBlockStep);
+      gridBlocks[row, column].Left := leftImageOffset + column *
+        (gridBlocks[row, column].Width + horizontalBlockStep);
+      gridBlocks[row, column].Stretch := true;
+      gridBlocks[row, column].Proportional := true;
+      gridBlocks[row, column].Name := 'block' + inttostr(blockNumber);
+      blockNumber := blockNumber + 1;
+      gridBlocks[row, column].Picture.LoadFromFile(pic);
+      // // imgs[c].OnClick := imgClick;
+      gridBlocks[row, column].Picture.LoadFromFile(pic);
+      gridBlocks[row, column].Visible := true;
+      gridBlocks[row, column].Refresh;
     end;
   end;
-  // (Image1.Picture.Graphic as TGIFImage).Animate := True;
+  for i := 0 to 5 do
+  begin
+    verticalCorridors[i] :=
+      round(gridBlocks[5, i].Left +
+      ((2 * gridBlocks[5, i].Width + verticalBlockStep) / 2) - (shipWidth / 2));
+  end;
+  for i := 0 to 4 do
+  begin
+    horizontalCorridors[i] :=
+      round(gridBlocks[i, 4].Top +
+      ((2 * gridBlocks[i, 4].Width + horizontalBlockStep) / 2) -
+      (shipWidth / 2));
+  end;
+  endLeft := verticalCorridors[0];
+  endRight := verticalCorridors[5];
+  endUp := horizontalCorridors[0];
+  endDown := horizontalCorridors[4];
+  moveStep := 5;
+  ep[0] := ep1;
+  ep[1] := ep2;
+  (Ship.Picture.Graphic as TGIFImage).Animate := true;
+  (ep[0].Picture.Graphic as TGIFImage).Animate := true;
+  (ep[1].Picture.Graphic as TGIFImage).Animate := true;
   shipX := verticalCorridors[3];
   shipY := horizontalCorridors[4];
+  Ship.Left := shipX;
+  Ship.Top := shipY;
+  epX[0] := verticalCorridors[3];
+  epY[0] := horizontalCorridors[4];
+  epX[1] := verticalCorridors[1];
+  epY[1] := horizontalCorridors[4];
   Timer1.Enabled := true;
-  Timer2.Enabled := true;
+  // Timer2.Enabled := true;
 end;
 
 procedure TForm1.FormKeyPress(Sender: TObject; var Key: Char);
 begin
-  if (Key = moveLeft) then
+ Form1.Caption := moveUp + ' ' + moveDown + ' ' + moveLeft + ' ' + moveRight;
+  if (Key = moveLeft1) then
   begin
-    lastMoveKey := moveLeft;
+    lastMoveKey := moveLeft1;
   end;
-  if (Key = moveRight) then
+  if (Key = moveRight1) then
   begin
-    lastMoveKey := moveRight;
+    lastMoveKey := moveRight1;
   end;
   if (Key = moveUp) then
   begin
@@ -154,43 +179,36 @@ begin
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
+
 var
-  row, column, image: integer;
+  i: integer;
 begin
-  for row := 0 to 5 do
+  randomize;
+  ((FindComponent('block' + inttostr(Random(42))) As TImage)
+    .Picture.Graphic as TGIFImage).Animate := true;
+  Timer1.Tag := 1;
+  for i := 0 to 41 do
   begin
-    for column := 0 to 6 do
+    if (((FindComponent('block' + inttostr(i)) As TImage)
+      .Picture.Graphic as TGIFImage).Animate = FALSE) then
     begin
-      for image := 0 to 39 do
-      begin
-        if image <> imgTimers[row, column] then
-        begin
-          gridImages[row, column, image].Visible := FALSE;
-          gridImages[row, column, image].Refresh;
-        end
-        else
-        begin
-          gridImages[row, column, image].Visible := true;
-          gridImages[row, column, imgTimers[row, column]].Refresh;
-        end;
-      end;
-      if imgTimers[row, column] = 39 then
-        imgTimers[row, column] := 1
-      else
-        imgTimers[row, column] := imgTimers[row, column] + 1;
+      Timer1.Tag := 0;
+      break;
     end;
   end;
+  if Timer1.Tag = 1 then
+    Timer1.Enabled := FALSE;
 end;
 
 procedure TForm1.Timer2Timer(Sender: TObject);
 begin
-  if (currentMoveKey = moveLeft) and (shipX > endLeft) and inHorizontalCorridor()
-  then
+  if (currentMoveKey = moveLeft1) and (shipX > endLeft) and
+    inHorizontalCorridor() then
   begin
     shipX := shipX - moveStep;
   end;
-  if (currentMoveKey = moveRight) and (shipX < endRight) and inHorizontalCorridor()
-  then
+  if (currentMoveKey = moveRight1) and (shipX < endRight) and
+    inHorizontalCorridor() then
   begin
     shipX := shipX + moveStep;
   end;
@@ -203,23 +221,77 @@ begin
   begin
     shipY := shipY + moveStep;
   end;
-  if ((lastMoveKey=moveLeft) or (lastMoveKey=moveRight)) and inHorizontalCorridor() then
+  if ((lastMoveKey = moveLeft1) or (lastMoveKey = moveRight1)) and
+    inHorizontalCorridor() then
   begin
-     currentMoveKey:=lastMoveKey;
+    currentMoveKey := lastMoveKey;
   end;
-  if ((lastMoveKey=moveUp) or (lastMoveKey=moveDown)) and inVerticalCorridor() then
+  if ((lastMoveKey = moveUp) or (lastMoveKey = moveDown)) and
+    inVerticalCorridor() then
   begin
-     currentMoveKey:=lastMoveKey;
+    currentMoveKey := lastMoveKey;
   end;
-  if (lastMoveKey=moveStop) and inHorizontalCorridor() and inVerticalCorridor() then
+  if (lastMoveKey = moveStop) and inHorizontalCorridor() and inVerticalCorridor()
+  then
   begin
-     currentMoveKey:=lastMoveKey;
+    currentMoveKey := lastMoveKey;
   end;
   Ship.Left := shipX;
   Ship.Top := shipY;
+  // Form1.Caption := inttostr(shipX) + ' / ' + inttostr(shipY);
+end;
+
+procedure TForm1.Timer3Timer(Sender: TObject);
+var
+  i: integer;
+begin
+  for i := 0 to 1 do
+  begin
+    if (ep[i].Tag = 1) and (epX[i] > endLeft) and { left }
+      enemyInHorizontalCorridor(i) then
+    begin
+      epX[i] := epX[i] - moveStep;
+    end;
+    if (ep[i].Tag = 2) and (epX[i] < endRight) and { right }
+      enemyInHorizontalCorridor(i) then
+    begin
+      epX[i] := epX[i] + moveStep;
+    end;
+    if (ep[i].Tag = 3) and (epY[i] > endUp) and
+      enemyInVerticalCorridor(i) { up }
+    then
+    begin
+      epY[i] := epY[i] - moveStep;
+    end;
+    if (ep[i].Tag = 4) and (epY[i] < endDown) and
+      enemyInVerticalCorridor(i) { down }
+    then
+    begin
+      epY[i] := epY[i] + moveStep;
+    end;
+    if ((lastMove[i] = 1) or (lastMove[i] = 2)) and enemyInHorizontalCorridor(i)
+    then
+    begin
+      ep[i].Tag := lastMove[i];
+    end;
+    if ((lastMove[i] = 3) or (lastMove[i] = 4)) and enemyInVerticalCorridor(i)
+    then
+    begin
+      ep[i].Tag := lastMove[i];
+    end;
+    if (lastMove[i] = 5) and enemyInHorizontalCorridor(i) and
+      enemyInVerticalCorridor(i) then
+    begin
+      ep[i].Tag := lastMove[i];
+    end;
+    ep[i].Left := epX[i];
+    ep[i].Top := epY[i];
+    // Form1.Caption := inttostr(shipX) + ' / ' + inttostr(shipY);
+  end;
 end;
 
 function TForm1.inHorizontalCorridor(): boolean;
+
 var
   i: integer;
 begin
@@ -230,11 +302,13 @@ begin
       inHorizontalCorridor := true;
       break;
     end
-    else inHorizontalCorridor:=false;
+    else
+      inHorizontalCorridor := FALSE;
   end;
 end;
 
 function TForm1.inVerticalCorridor(): boolean;
+
 var
   i: integer;
 begin
@@ -245,9 +319,55 @@ begin
       inVerticalCorridor := true;
       break;
     end
-    else  inVerticalCorridor:=false;
+    else
+      inVerticalCorridor := FALSE;
   end;
 
+end;
+
+function TForm1.enemyInHorizontalCorridor(enemy: integer): boolean;
+var
+  i: integer;
+begin
+  for i := 0 to 4 do
+  begin
+    if epY[enemy] = horizontalCorridors[i] then
+    begin
+      enemyInHorizontalCorridor := true;
+      break;
+    end
+    else
+      enemyInHorizontalCorridor := FALSE;
+  end;
+end;
+
+function TForm1.enemyInVerticalCorridor(enemy: integer): boolean;
+
+var
+  i: integer;
+begin
+  for i := 0 to 5 do
+  begin
+    if epX[enemy] = verticalCorridors[i] then
+    begin
+      enemyInVerticalCorridor := true;
+      break;
+    end
+    else
+      enemyInVerticalCorridor := FALSE;
+  end;
+
+end;
+
+procedure TForm1.enemyMoveTimer(Sender: TObject);
+var
+  i: integer;
+begin
+  for i := 0 to 11 do
+  begin
+    randomize;
+    lastMove[i] := 1 + Random(4);
+  end;
 end;
 
 end.
